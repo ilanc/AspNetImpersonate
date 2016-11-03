9b0cdb5cbdeeac3c29c82109955a13b4551339f5
Ilan Copelyn <ilan@netreturn.co.za>
2016-11-03 11:45:03 +0200

    Impersonate

--- WebApplicationNet462/Controllers/HomeController.cs
+++ WebApplicationNet462/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+using System.Data.SqlClient;
 
 namespace WebApplicationNet462.Controllers
 {
@@ -16,7 +18,11 @@ namespace WebApplicationNet462.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            var callerIdentity = User.Identity as WindowsIdentity;
+            WindowsIdentity.RunImpersonated(callerIdentity.AccessToken, () => {
+                ViewData["Name"] = ($"{WindowsIdentity.GetCurrent().Name}!");
+                ViewData["List"] = sql_vData_BloombergRequest("Data Source=IAMGBLSQLUAT2;Initial Catalog=InfoPortal;Integrated Security=SSPI;");
+            });
             return View();
         }
 
@@ -30,6 +36,40 @@ namespace WebApplicationNet462.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        public class BBGDataLicenseJson
+        {
+            public string Ticker { get; set; }
+            public string Mnemonic { get; set; }
+            public BBGDataLicenseJson(SqlDataReader reader)
+            {
+                Ticker = Convert.IsDBNull(reader[0]) ? null : reader.GetString(0);
+                Mnemonic = Convert.IsDBNull(reader[1]) ? null : reader.GetString(1);
+            }
+        }
+
+        public static List<BBGDataLicenseJson> sql_vData_BloombergRequest(string connectionString)
+        {
+            var sql =
+@"select external_value as Ticker, internal_value as Mnemonic 
+from infoportal_staging.dbo.tbl_CrossReference
+where originator_id = 'BloombergRequest' and internal_value2 is null";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sql, connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    var results = new List<BBGDataLicenseJson>(100);
+                    while (reader.Read())
+                    {
+                        results.Add(new BBGDataLicenseJson(reader));
+                    }
+                    return results;
+                }
+            }
         }
     }
 }
